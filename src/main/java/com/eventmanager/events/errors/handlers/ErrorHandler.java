7359c478d1c3799f8530699d85922098ad5823c6
Kingsley Victor <javaprodigy56@gmail.com>
2020-01-05 19:11:29 +0100

    Update error handler

--- src/main/java/com/eventmanager/events/errors/handlers/ErrorHandler.java
+++ src/main/java/com/eventmanager/events/errors/handlers/ErrorHandler.java
@@ -3,6 +3,7 @@ package com.eventmanager.events.errors.handlers;
 import com.eventmanager.events.errors.Error;
 import com.eventmanager.events.responses.Response;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,7 @@ public class ErrorHandler {
   public ResponseEntity<Response<String>> exception(Error error) {
     return new ResponseEntity<>(
       new Response<>(error.getStatusCode(), error.getMessage()),
-      ResponseEntity.status(error.getStatusCode()).build().getStatusCode()
+      HttpStatus.valueOf(error.getStatusCode())
     );
   }
 }
