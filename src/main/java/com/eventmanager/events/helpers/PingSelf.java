6dbac1048b0df25becabd03b99cc78309b77dff6
Kingsley Victor <javaprodigy56@gmail.com>
2020-01-11 14:23:14 +0100

    Uncommented code in PingSelf.java

--- src/main/java/com/eventmanager/events/helpers/PingSelf.java
+++ src/main/java/com/eventmanager/events/helpers/PingSelf.java
@@ -1,16 +1,16 @@
-// package com.eventmanager.events.helpers;
+package com.eventmanager.events.helpers;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.scheduling.annotation.Scheduled;
-// import org.springframework.stereotype.Component;
-// import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-// @Component
-// public class PingSelf {
-//   @Autowired RestTemplate template;
+@Component
+public class PingSelf {
+  @Autowired RestTemplate template;
 
-//   @Scheduled(fixedRate = (60000 * 5))
-//   public void run() {
-//     template.getForEntity("https://event-manager-main.herokuapp.com", Object.class);
-//   }
-// }
+  @Scheduled(fixedRate = (60000 * 5))
+  public void run() {
+    template.getForEntity("https://event-manager-main.herokuapp.com", Object.class);
+  }
+}
